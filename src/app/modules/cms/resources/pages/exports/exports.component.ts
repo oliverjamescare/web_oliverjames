cf2a3346a283dac900886d3fdf98df8429bd4ede
Karol Ludwikowski <karol.ludwikowski@ready4s.pl>
2018-06-07 16:32:33 +0200

    exports and validations in parameters

--- src/app/modules/cms/resources/pages/exports/exports.component.ts
+++ src/app/modules/cms/resources/pages/exports/exports.component.ts
@@ -26,6 +26,36 @@ export class ExportsComponent implements OnInit {
     private endpoint: string;
     private webEndpoint: string;
 
+
+    messages = [
+        {
+            field: 'from',
+            errors: [
+                {
+                    error: 'required',
+                    message: 'Date is required'
+                },
+                {
+                    error: 'invalidDate',
+                    message: 'Date format is not valid'
+                }
+            ]
+        },
+        {
+            field: 'to',
+            errors: [
+                {
+                    error: 'required',
+                    message: 'Date is required'
+                },
+                {
+                    error: 'invalidDate',
+                    message: 'Date format is not valid'
+                }
+            ]
+        }
+    ];
+
     constructor(private authService: AuthService, private fb: FormBuilder, private notificationService: NotificationsService) {
         this.endpoint = environment.admin;
         this.webEndpoint = environment.api;
@@ -47,12 +77,20 @@ export class ExportsComponent implements OnInit {
             }
         }
         if (this.formExport.valid) {
+            let fromDate, toDate;
             if (this.formExport.value.from)
-                this.formExport.value.from = moment(this.formExport.value.from).format('YYYY-MM-DD');
+                fromDate = moment(this.formExport.value.from).format('YYYY-MM-DD');
             if (this.formExport.value.to)
-                this.formExport.value.to = moment(this.formExport.value.to).format('YYYY-MM-DD');
+                toDate = moment(this.formExport.value.to).format('YYYY-MM-DD');
             const accessToken = this.authService.getAccessToken();
             this.linkToCsvFile = this.endpoint + '/exports/' + this.formExport.value.collection + '?access-token=' + accessToken;
+            if (toDate)
+                this.linkToCsvFile = this.endpoint + '/exports/' + this.formExport.value.collection + '?access-token=' + accessToken + '&to=' + toDate;
+            if (fromDate)
+                this.linkToCsvFile = this.endpoint + '/exports/' + this.formExport.value.collection + '?access-token=' + accessToken + '&from=' + fromDate;
+            if (toDate && fromDate)
+                this.linkToCsvFile = this.endpoint + '/exports/' + this.formExport.value.collection + '?access-token=' + accessToken + '&from=' + fromDate + '&to=' + toDate;
+
             const win = window.open(this.linkToCsvFile, '_blank');
             win.focus();
         }
